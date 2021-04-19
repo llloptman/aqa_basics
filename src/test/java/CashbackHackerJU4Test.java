b3dabb783dfcbcee8a8a8f897513a25d19d52d8e
LLlOPT <llloptman@gmail.com>
2021-04-19 12:04:38 +0300

    Junit4

--- src/test/java/CashbackHackerJU4Test.java
+++ src/test/java/CashbackHackerJU4Test.java
@@ -1,35 +1,35 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-public class CashbackHackerTest {
+public class CashbackHackerJU4Test {
     CashbackHacker cashbackHacker = new CashbackHacker();
 
     @Test
     public void shouldReturn_0() {
         int actual = cashbackHacker.remain(2000);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void shouldReturn_100() {
         int actual = cashbackHacker.remain(900);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void shouldReturn_1000() {
         int actual = cashbackHacker.remain(0);
         int expected = 1000;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void shouldntWorkWithNegative() {
         int actual = cashbackHacker.remain(-100);
         int expected = -1;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 }
